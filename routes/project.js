0ac1cbccfc776db0bb9e336750bfe930b72cf615
Adnan <adnan@lakhaniproductions.com>
2020-10-27 20:06:24 -0400

    Update project.js

--- routes/project.js
+++ routes/project.js
@@ -1,8 +1,19 @@
 const express= require('express');
 const router = express.Router();
 
+const data = require('../data.json').data;
+
+const projects=data.projects;
+
+
+
 router.get('/project/:id', (req,res) => {
-    res.render('project');
+    res.render('project', {
+        project:projects[req.params.id],
+        title:projects[req.params.id].project_name,
+        description:projects[req.params.id].description,
+        technologies:projects[req.params.id].technologies
+    });
 });
 
 module.exports = router;
